7ec663de9496c6ac9c8d1c34aaebbe651c36cc29
Kane Rogers <kane.m.rogers@gmail.com>
2017-04-15 19:24:23 +1000

    Initial commit.

---
+++ src/NavBar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {THEME_GREY, WHITE, LIGHT_GREY} from './colours';
+
+const styles = {
+  navBar: {
+    backgroundColor: THEME_GREY,
+    height: 79,
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginButtom: 1,
+  },
+  innerContainer: {
+    display: 'flex',
+    height: 79,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 17,
+    paddingRight: 17,
+  },
+  activeIcon: {
+    fontSize: 33.85,
+    color: WHITE,
+    marginRight: 18,
+  },
+  inactiveIcon: {
+    fontSize: 33.85,
+    color: LIGHT_GREY,
+    marginRight: 18,
+  },
+};
+import logo from './logo.png';
+
+export default () => (
+  <div style={styles.navBar}>
+    <div style={styles.innerContainer}>
+      <img alt="Balanced Logo" src={logo} />
+    </div>
+    <div style={styles.innerContainer}>
+      <i style={styles.activeIcon} className="fa fa-bars" />
+      <i style={styles.inactiveIcon} className="fa fa-facebook" />
+      <i style={styles.inactiveIcon} className="fa fa-twitter" />
+    </div>
+  </div>
+);
---
+++ src/colours.js
@@ -0,0 +1,3 @@
+export const THEME_GREY = '#2b2b2b';
+export const WHITE = '#ffffff';
+export const LIGHT_GREY = '#444444';
