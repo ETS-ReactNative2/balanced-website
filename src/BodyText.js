68e4db134e1cc6d8f4c812d5ece92c4a7eec4f1e
Kane Rogers <kane.m.rogers@gmail.com>
2017-04-21 16:41:08 +1000

    Make OurStory a little cleaner.

--- src/BodyText.js
+++ src/BodyText.js
@@ -1,10 +1,4 @@
 import React from 'react';
 import './BodyText.css';
 
-const getClassName = textAlign => {
-  return 'BodyText ' + (textAlign === 'left' ? 'AlignLeft' : '');
-};
-
-export default ({children, textAlign}) => (
-  <p className={getClassName(textAlign)}>{children}</p>
-);
+export default ({children}) => <p className="BodyText">{children}</p>;
