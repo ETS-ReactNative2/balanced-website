3125cc8c914658bd6693f379de17599577874719
Kane Rogers <kane.m.rogers@gmail.com>
2017-04-21 14:09:14 +1000

    Make Intro mobile friendly.

--- src/Intro.js
+++ src/Intro.js
@@ -1,45 +1,16 @@
 import React from 'react';
-import * as styles from './styles';
 import {THEME_GREEN} from './colours';
-
-const style = {
-  outerContainer: {
-    marginTop: '110px',
-    display: 'flex',
-    flex: 1,
-    justifyContent: 'center',
-    color: '#2b2b2b',
-  },
-  header: {
-    fontFamily: 'Open Sans, sans-serif',
-    color: THEME_GREEN,
-    fontWeight: 600,
-    textAlign: 'center',
-    lineHeight: '50px',
-    fontSize: '30px',
-    marginBottom: 67,
-  },
-  subheader: {
-    margin: 0,
-    fontFamily: 'Open Sans, sans-serif',
-    textAlign: 'center',
-    lineHeight: '40px',
-    fontSize: '25px',
-    fontWeight: 400,
-    paddingLeft: 67,
-    paddingRight: 67,
-  },
-};
+import './Intro.css';
 
 export default () => (
-  <div id="About_Container" style={style.outerContainer}>
-    <div style={styles.innerContainer}>
-      <h1 style={style.header}>
+  <div id="Intro_Container">
+    <div id="Intro_InnerContainer">
+      <h1 id="Intro_Header">
         For too long, too many major food corporations have prioritized their profits over our health.
         They’ve ignored federal dietary guidelines and the best nutrition evidence, and our families have paid the price—with
         diabetes, high blood pressure, heart disease, and premature death.
       </h1>
-      <h2 style={style.subheader}>
+      <h2 id="Intro_Subheader">
         These companies have created a public health crisis and blamed it on us, just like big tobacco did two generations ago.
         Like big tobacco, they’ve called consumer health an individual choice—not a corporate responsibility.
         The truth is, we’ve been set up to fail. When cafeterias, restaurants, and grocery aisles are overstocked with
