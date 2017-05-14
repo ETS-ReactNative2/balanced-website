3ae41ef1ae48c4b0eacf96a170e1d2ee6a78a1d4
Kane Rogers <kane.m.rogers@gmail.com>
2017-05-14 16:48:07 +1000

    Paragraph AdvisoryBio text

--- src/AdvisoryBio.js
+++ src/AdvisoryBio.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import BodyText from './BodyText';
 import './AdvisoryBio.css';
-import {BIOS} from './advisory_board_constants';
+import { BIOS } from './advisory_board_constants';
 
-export default ({selectedName}) => (
+export default ({ selectedName }) => (
   <div id="AdvisoryBio_Container">
-    <BodyText>{BIOS[selectedName]}</BodyText>
+    {BIOS[selectedName].split('\n').map(text => <BodyText>{text}</BodyText>)}
   </div>
 );
