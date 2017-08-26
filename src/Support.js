e855e944874ad55cfd413be288006116341f498f
Simbidion <simonadwyer@yahoo.com.au>
2017-08-26 16:40:25 +1000

    Add citations to support
    Added super script citations with links. Css for a tags is in
    Support.css. White goes to a light grey on visited.

--- src/Support.js
+++ src/Support.js
@@ -16,10 +16,10 @@ export default () =>
       <SectionHeader>WE ALREADY HAVE THE SUPPORT</SectionHeader>
       <BodyText>
         This common sense change is supported by an array of public health
-        organizations like the <strong>Obesity Action Coalition</strong> and{' '}
-        <strong>The Defeat Diabetes Foundation</strong>
+        organizations like the <strong>Obesity Action Coalition</strong><sup><a href="http://www.obesityaction.org/educational-resources/resource-articles-2/nutrition/rethink-your-plate">1</a></sup> and{' '}
+        <strong>The Defeat Diabetes Foundation</strong><sup><a href="https://www.defeatdiabetes.org/myplate/">2</a></sup>
         , and leading health care providers like{' '}
-        <strong>Kaiser Permanente</strong> and <strong>The Mayo Clinic</strong>
+        <strong>Kaiser Permanente</strong><sup><a href="https://share.kaiserpermanente.org/wp-content/uploads/2015/10/The-Plant-Based-Diet-booklet.pdf">3</a></sup> and <strong>The Mayo Clinic</strong><sup><a href="http://www.mayoclinic.org/healthy-lifestyle/nutrition-and-healthy-eating/in-depth/dietary-guidelines/art-20045584">4</a></sup>
         .
       </BodyText>
       <div className="Support_logos">
