193dc5dad4b6a775e76967c6eb232a6207cbdbb8
Kane Rogers <kane.m.rogers@gmail.com>
2017-09-13 19:35:33 +1000

    Add link to Drop Aramark

--- src/balanced_plate_alliance/Campaigns.js
+++ src/balanced_plate_alliance/Campaigns.js
@@ -6,6 +6,8 @@ import './Campaigns.css';
 export default () => (
   <div id="Campaigns">
     <h1>CURRENT CAMPAIGNS</h1>
-    <img alt="Aramark Logo" src={aramark} />
+    <a href="https://www.droparamark.com/">
+      <img alt="Aramark Logo" src={aramark} />
+    </a>
   </div>
 );
