b74ef4c997d9b46aa50d790e770cd16690f3f93e
Kane Rogers <kane.m.rogers@gmail.com>
2017-09-13 16:45:03 +1000

    Add links to social icons

--- src/balanced_plate_alliance/Share.js
+++ src/balanced_plate_alliance/Share.js
@@ -9,8 +9,12 @@ export default () => (
     <h1>SHARE</h1>
     <span>Let your friends know how they can get involved</span>
     <div id="ShareIcons">
-      <img src={facebook} />
-      <img src={twitter} />
+      <a href="https://www.facebook.com/sharer/sharer.php?u=http%3A//balanced.org/bpa">
+        <img src={facebook} />
+      </a>
+      <a href="https://twitter.com/home?status=Healthy%20food%20is%20a%20right,%20and%20we're%20going%20to%20fight%20for%20it%3A%20http%3A//balanced.org/bpa">
+        <img src={twitter} />
+      </a>
     </div>
   </div>
 );
