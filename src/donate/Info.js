c1d369f1ebf4cf8c62d025d38c8baa8f65a5c9d9
Kane Rogers <kane.m.rogers@gmail.com>
2017-10-19 15:26:29 +1100

    WIP - Form

--- src/donate/Info.js
+++ src/donate/Info.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Info.css";
 
 const Info = () => (
-  <div>
+  <div id="Donate_Info">
     <form>
       <h5>Contact Information</h5>
-      <input type="text" placeholder="First name" />
-      <input type="text" placeholder="Last name" />
-      <input type="text" placeholder="Email" />
-      <input type="text" placeholder="Phone" />
+      <input name="first_name" type="text" placeholder="First name" />
+      <input name="last_name" type="text" placeholder="Last name" />
+      <input name="email" type="text" placeholder="Email" />
+      <input name="phone" type="text" placeholder="Phone" />
 
       <h5>Billing Information</h5>
-      <input type="text" placeholder="Address" />
-      <input type="text" placeholder="Address 2" />
-      <input type="text" placeholder="Address 3" />
-      <input type="text" placeholder="City" />
-      <input type="text" placeholder="State" />
-      <input type="text" placeholder="Zip Code" />
-      <input type="text" placeholder="Country" />
+      <input name="address" type="text" placeholder="Address" />
+      <input name="address_2" type="text" placeholder="Address 2" />
+      <input name="address_3" type="text" placeholder="Address 3" />
+      <input name="city" type="text" placeholder="City" />
+      <input name="state" type="text" placeholder="State" />
+      <input name="zip" type="text" placeholder="Zip Code" />
+      <input name="country" type="text" placeholder="Country" />
     </form>
 
     <Link to="/donate/payment">NEXT</Link>
