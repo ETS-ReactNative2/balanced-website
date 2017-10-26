b0b6129058ea5ba347f8f4b8fcc7d478bb35b8bd
Kane Rogers <kane.m.rogers@gmail.com>
2017-10-26 19:14:07 +1100

    Finish makiung Info desktop friendly

--- src/donate/info/index.js
+++ src/donate/info/index.js
@@ -11,30 +11,36 @@ const Info = ({ previousStep, nextStep, values }) => (
         return (
           <form id="Donate_Info" onSubmit={submitForm}>
             <h5>Contact Information</h5>
-            <Text field="first_name" placeholder="First name" />
-            <Text field="last_name" placeholder="Last name" />
-            <Text field="email" placeholder="Email" />
-            <Text field="phone" placeholder="Phone" />
+            <div className="Donate_FormGroup">
+              <Text field="first_name" placeholder="First name" />
+              <Text field="last_name" placeholder="Last name" />
+              <Text field="email" placeholder="Email" />
+              <Text field="phone" placeholder="Phone" />
+            </div>
 
             <h5>Billing Information</h5>
-            <Text field="address" placeholder="Address" />
-            <Error errors={errors} touched={touched} fieldName={"address"} />
+            <div className="Donate_FormGroup">
+              <Text field="address" placeholder="Address" />
+              <Error errors={errors} touched={touched} fieldName={"address"} />
 
-            <Text field="address_2" placeholder="Address 2" />
+              <Text field="address_2" placeholder="Address 2" />
 
-            <Text field="address_3" placeholder="Address 3" />
+              <Text field="address_3" placeholder="Address 3" />
 
-            <Text field="city" placeholder="City" />
-            <Error errors={errors} touched={touched} fieldName={"city"} />
+              <Text field="city" placeholder="City" />
+              <Error errors={errors} touched={touched} fieldName={"city"} />
 
-            <Text field="state" placeholder="State" />
-            <Error errors={errors} touched={touched} fieldName={"state"} />
+              <Text field="state" placeholder="State" />
+              <Error errors={errors} touched={touched} fieldName={"state"} />
 
-            <Text field="zip" placeholder="Zip Code" />
-            <Error errors={errors} touched={touched} fieldName={"zip"} />
+              <Text field="zip" placeholder="Zip Code" />
+              <Error errors={errors} touched={touched} fieldName={"zip"} />
 
-            <Text field="country" placeholder="Country" />
-            <Error errors={errors} touched={touched} fieldName={"country"} />
+              <Text field="country" placeholder="Country" />
+              <Error errors={errors} touched={touched} fieldName={"country"} />
+            </div>
+
+            <span className="Donate_Encouragement">Almost done!</span>
 
             <div className="Donate_ButtonGroup">
               <button
