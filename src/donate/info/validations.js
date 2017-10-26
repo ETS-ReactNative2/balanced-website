4ea29a74432a44b15ae9eded8a5e2a123f04efe5
Kane Rogers <kane.m.rogers@gmail.com>
2017-10-26 16:09:29 +1100

    Finish validating Info

--- src/donate/info/validations.js
+++ src/donate/info/validations.js
@@ -1,3 +1,7 @@
-export default values => ({
-  address: "Address is required"
+export default ({ address, city, state, zip, country }) => ({
+  address: address ? null : "Please enter your address",
+  city: city ? null : "Please enter your city",
+  state: state ? null : "Please enter your state",
+  zip: zip ? null : "Please enter your ZIP code",
+  country: country ? null : "Please select your countryAc"
 });
