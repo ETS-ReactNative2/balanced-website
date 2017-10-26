7b3f34b8c255661f768331d8629bb36ffbaa8239
Kane Rogers <kane.m.rogers@gmail.com>
2017-10-26 18:31:49 +1100

    Begin work on desktop version.

--- src/donate/amount/AmountSelector.js
+++ src/donate/amount/AmountSelector.js
@@ -2,26 +2,17 @@ import React from "react";
 import { FormField } from "react-form";
 import Amount from "./Amount";
 
-const TOP_AMOUNTS = [10, 40, 100];
-const BOTTOM_AMOUNTS = [500, "OTHER"];
+const TOP_AMOUNTS = [10, 40, 100, 500, "OTHER"];
 
 const AmountSelector = ({ fieldApi, ...rest }) => {
   const props = { ...fieldApi, rest };
   const { getValue } = fieldApi;
   return (
     <div>
-      <div className="Donate_AmountRow">
+      <div id="Donate_Amounts">
         {TOP_AMOUNTS.map(a => (
           <Amount key={a} amount={a} selected={getValue() === a} {...props} />
         ))}
-      </div>
-
-      <div className="Donate_AmountRow">
-        <div className="Donate_Space" />
-        {BOTTOM_AMOUNTS.map(a => (
-          <Amount amount={a} key={a} selected={getValue() === a} {...props} />
-        ))}
-        <div className="Donate_Space" />
       </div>
     </div>
   );
